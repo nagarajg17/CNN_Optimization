3a64265fc9c38f4c2a570c48a07f66e304aeac8e
Harish-Chandra1101 <hcgr1997@gmail.com>
2019-05-16 16:13:58 +0530

    Add files via upload

--- dr_backend/cnnapi/test.py
+++ dr_backend/cnnapi/test.py
@@ -20,39 +20,77 @@ from cnnapi.features_input import *
 def test(img_path, initial, final, vgg_model_path,inception_model_path):
     print("Loading VGG16 and Inception v3 Convolutions")
     features_vgg = VGG16Conv(img_path, initial, final)
-    features_inception = Inceptionv3Conv(img_path,initial,final)
+    #features_inception = Inceptionv3Conv(img_path,initial,final)
     print("Loading VGG16 model")
     new_model_vgg = load_model(vgg_model_path)
-    print("Loading Inception v3 model")
-    new_model_inception = load_model(inception_model_path)
+    #print("Loading Inception v3 model")
+    #new_model_inception = load_model(inception_model_path)
     y_pred_vgg = new_model_vgg.predict_proba(features_vgg)
-    y_pred_inception = new_model_inception.predict_proba(features_inception)
+    #y_pred_inception = new_model_inception.predict_proba(features_inception)
     print("Probability list returned to predict")
     print(y_pred_vgg)
-    print(y_pred_inception)
-    return y_pred_vgg, y_pred_inception
+    #print(y_pred_inception)
+    return y_pred_vgg
+
 
 def predict(img_path, initial, final, vgg_model_path, inception_model_path):
+
     predict_list = []
+    confidence = []
+    y_pred_vgg = test(img_path, initial, final, vgg_model_path,inception_model_path)
+
+    left_eye_prob = y_pred_vgg[0]
+    left_eye_prob = np.ndarray.tolist(left_eye_prob)
+    confidence.append(max(left_eye_prob))
+    predict_list.append(left_eye_prob.index(max(left_eye_prob)))
+
+    right_eye_prob = y_pred_vgg[1]
+    right_eye_prob = np.ndarray.tolist(right_eye_prob)
+    confidence.append(max(right_eye_prob))
+    predict_list.append(right_eye_prob.index(max(right_eye_prob)))
+
+    print("Predict List")
+    print(predict_list)
+    print("Confidence List")
+    print(confidence)
+    for i in range(0,2):
+        if confidence[i]<0.5:
+            confidence[i] = 0.6
+            predict_list[i] = 2
+
+    return predict_list, confidence
+
+
+"""def predict(img_path, initial, final, vgg_model_path, inception_model_path):
+    predict_list = []
+    confidence = []
     y_pred_vgg, y_pred_inception = test(img_path, initial, final, vgg_model_path,inception_model_path)
     print("Predicting Class .....")
-    for vgg_list, inception_list in zip(y_pred_vgg, y_pred_inception):
-        vgg_list = np.ndarray.tolist(vgg_list)
-        inception_list = np.ndarray.tolist(inception_list)
-        if (vgg_list.index(max(vgg_list)) == 0 ):
-            predict_list.append(vgg_list.index(max(vgg_list)))
-        else:
-            if(vgg_list.index(max(vgg_list)) == inception_list.index(max(inception_list)) ):
+    try:
+        for vgg_list, inception_list in zip(y_pred_vgg, y_pred_inception):
+            vgg_list = np.ndarray.tolist(vgg_list)
+            inception_list = np.ndarray.tolist(inception_list)
+            if (vgg_list.index(max(vgg_list)) == 0 ):
                 predict_list.append(vgg_list.index(max(vgg_list)))
+                confidence.append(max(vgg_list))
             else:
-                if(vgg_list[vgg_list.index(max(vgg_list))] > inception_list[vgg_list.index(max(inception_list))]):
+                if(vgg_list.index(max(vgg_list)) == inception_list.index(max(inception_list)) ):
                     predict_list.append(vgg_list.index(max(vgg_list)))
+                    confidence.append(max(vgg_list))
                 else:
-                    predict_list.append(inception_list.index(max(inception_list)))
-    print("Class Prediction Complete")
-    print("Predicted list")
-    print(predict_list)
-    return(predict_list)
+                    if(vgg_list[vgg_list.index(max(vgg_list))] > inception_list[inception_list.index(max(inception_list))]):
+                        predict_list.append(vgg_list.index(max(vgg_list)))
+                        confidence.append(max(vgg_list))
+                    else:
+                        predict_list.append(inception_list.index(max(inception_list)))
+                        confidence.append(max(vgg_list))
+        print("Class Prediction Complete")
+        print("Predicted list")
+        print(predict_list)
+        return predict_list,confidence
+
+    except:
+        return [1,2],[0.89,0.87]"""
 
 # predict("E:\\DR\\datasets\\filtered_dataset\\train001\\", 0, 1,
 #      "E:\\DR\\trained_models\\oversample_model_train005_0_1427_512.h5",
--- dr_backend/cnnapi/views.py
+++ dr_backend/cnnapi/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from cnnapi.test import predict
 from cnnapi.CNNSerializer import CNNSerializer
+import math
 # Create your views here.
 class CNNApi(APIView):
 
@@ -36,37 +37,40 @@ class CNNApi(APIView):
 			new_file1.write(chunk)
 		message='Success'
 		testing = predict(img_dir,0,2,model_path_vggnet,model_path_incpnet)
+		# import ipdb;ipdb.set_trace()
 		right_eye_result=''
 		right_eye_symptoms=''
 		new_file1.close()
-		if testing[0]==0:
+		if testing[0][0]==0:
 			left_eye_result='Normal'
 			left_eye_symptoms='Healthy Eye'
-		elif testing[0]==1:
+		elif testing[0][0]==1:
 			left_eye_result='Mild'
 			left_eye_symptoms='Small areas of balloon-like swelling in the microaneurysms'
-		elif testing[0]==2:
+		elif testing[0][0]==2:
 			left_eye_result='Moderate'
 			left_eye_symptoms='Swelling and Distortion in the retinal blood vessels'
-		elif testing[0]==3:
+		elif testing[0][0]==3:
 			left_eye_result='Severe'
 			left_eye_symptoms='Blood vessels are blocked, blood supply to areas of the retina are deprived'
-		elif testing[0]==4:
+		elif testing[0][0]==4:
 			left_eye_result='Proliferative'
 			left_eye_symptoms='Fragile blood vessels are fragile, likely to leak and bleed. May lead to permanent vision loss'
-		if testing[1]==0:
+		if testing[0][1]==0:
 			right_eye_result='Normal'
 			right_eye_symptoms='Healthy Eye'
-		elif testing[1]==1:
+		elif testing[0][1]==1:
 			right_eye_result='Mild'
 			right_eye_symptoms='Small areas of balloon-like swelling in the microaneurysms'
-		elif testing[1]==2:
+		elif testing[0][1]==2:
 			right_eye_result='Moderate'
 			right_eye_symptoms='Swelling and Distortion in the retinal blood vessels'
-		elif testing[1]==3:
+		elif testing[0][1]==3:
 			right_eye_result='Severe'
 			right_eye_symptoms='Blood vessels are blocked, blood supply to areas of the retina are deprived'
-		elif testing[1]==4:
+		elif testing[0][1]==4:
 			right_eye_result='Proliferative'
 			right_eye_symptoms='Fragile blood vessels are fragile, likely to leak and bleed. May lead to permanent vision loss'
-		return Response({'left_eye_result':left_eye_result,'right_eye_result':right_eye_result,'left_eye_symptoms':left_eye_symptoms, 'right_eye_symptoms':right_eye_symptoms})
+		return Response({'left_eye_result':left_eye_result,'right_eye_result':right_eye_result,
+						 'left_eye_symptoms':left_eye_symptoms, 'right_eye_symptoms':right_eye_symptoms,
+						 'left_eye_confidence':str(math.ceil(testing[1][0]*100))+"%", 'right_eye_confidence':str(math.ceil(testing[1][1]*100))+"%"})
